2539d711b6a446ba5ba64699b9a725317f8abab0
Harsimran Saini <harry.singh@mailbox.org>
2020-04-06 12:11:54 -0400

    Fix base url

--- network/src/main/java/com/harryio/fizz/network/NetworkInteractor.kt
+++ network/src/main/java/com/harryio/fizz/network/NetworkInteractor.kt
@@ -5,7 +5,7 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-private const val BASE_API_URL = "https://api.themoviedb.org/"
+private const val BASE_API_URL = "https://api.themoviedb.org/3/"
 
 class NetworkInteractor {
 
