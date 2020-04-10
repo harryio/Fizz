ab27919a376fe3019f581629f6f8a620f1897bab
Harsimran Saini <harry.singh@mailbox.org>
2020-04-10 13:35:32 -0400

    Show login fragment

--- app/src/main/java/com/harryio/fizz/MainActivity.kt
+++ app/src/main/java/com/harryio/fizz/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.harryio.fizz
 
-import androidx.fragment.app.FragmentActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : FragmentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
--- login/src/main/java/com/harryio/fizz/login/LoginFragment.kt
+++ login/src/main/java/com/harryio/fizz/login/LoginFragment.kt
@@ -1,7 +1,20 @@
 package com.harryio.fizz.login
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.harryio.fizz.login.databinding.FragmentLoginBinding
 
-class LoginFragment: Fragment() {
-    
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return FragmentLoginBinding.inflate(inflater, container, false).root
+    }
+
 }
