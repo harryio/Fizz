aa491fe24c8d79ed601e46e2716bb028751f9a55
Harsimran Saini <harry.singh@mailbox.org>
2020-04-11 17:59:22 -0400

    Add statuses for network calls

---
+++ domain/src/main/java/com/harryio/fizz/doman/Status.kt
@@ -0,0 +1,9 @@
+package com.harryio.fizz.doman
+
+enum class Status {
+
+    SUCCESS,
+    LOADING,
+    ERROR,
+    EMPTY
+}
--- login/src/main/java/com/harryio/fizz/login/LoginViewModel.kt
+++ login/src/main/java/com/harryio/fizz/login/LoginViewModel.kt
@@ -1,16 +1,45 @@
 package com.harryio.fizz.login
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.harryio.fizz.common_feature.BaseViewModel
+import com.harryio.fizz.doman.Resource
+import com.harryio.fizz.doman.Status
 import com.harryio.fizz.doman.authenticationUseCase
 
 internal class LoginViewModel : BaseViewModel() {
 
+    private val loginObserver: MutableLiveData<Resource<String>> = MutableLiveData(Resource.empty())
+
+    val loginButtonEnabled = Transformations.map(loginObserver) {
+        it.status != Status.LOADING
+    }
+
+
+    val loginButtonText = Transformations.map(loginObserver) {
+        if (it.status == Status.LOADING) {
+            R.string.logging_in
+        } else {
+            R.string.login
+        }
+    }
+
     fun handleLoginButtonClick() {
+        loginObserver.value = Resource.loading()
         disposables.add(
-            authenticationUseCase.getAuthenticationToken().subscribe(
-                {},
-                Throwable::printStackTrace
-            )
+            authenticationUseCase.getAuthenticationToken()
+                .subscribe(
+                    { authenticationToken ->
+                        loginObserver.postValue(Resource.success(authenticationToken.token))
+                    },
+                    { throwable ->
+                        loginObserver.postValue(
+                            Resource.error(
+                                throwable.message ?: "Something went wrong"
+                            )
+                        )
+                    }
+                )
         )
     }
 }
