5fb004c25e423df5328968699498eb69d9b71f2a
Harsimran Saini <harry.singh@mailbox.org>
2020-08-22 20:23:04 -0400

    Remove unrefrenced code and clean code

--- features/login/src/main/java/com/harryio/fizz/login/logindi.kt
+++ features/login/src/main/java/com/harryio/fizz/login/logindi.kt
@@ -1,13 +1,12 @@
 package com.harryio.fizz.login
 
 import com.harryio.fizz.authenticationrepository.AuthenticationModule
-import com.harryio.fizz.authenticationrepository.AuthenticationRepository
+import com.squareup.inject.assisted.dagger2.AssistedModule
 import dagger.Component
 import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import javax.inject.Provider
 
 @Component(modules = [LoginModule::class, AuthenticationModule::class])
 internal interface LoginComponent {
@@ -15,16 +14,9 @@ internal interface LoginComponent {
     fun loginViewModelFactory(): LoginViewModel.Factory
 }
 
-@Module
+@Module(includes = [AssistedInject_LoginModule::class])
+@AssistedModule
 internal object LoginModule {
-
-    @Provides
-    @JvmStatic
-    fun provideLoginViewModelFactory(
-        authenticationRepository: Provider<AuthenticationRepository>,
-        coroutineDispatcherProvider: Provider<CoroutineDispatcher>
-    ): LoginViewModel.Factory =
-        LoginViewModel_AssistedFactory(authenticationRepository, coroutineDispatcherProvider)
 
     @Provides
     @JvmStatic
--- repositories/common-repository/src/main/java/com/harryio/fizz/domain/Extensions.kt
+++ repositories/common-repository/src/main/java/com/harryio/fizz/domain/Extensions.kt
@@ -1,29 +1,12 @@
 package com.harryio.fizz.domain
 
 import com.harryio.fizz.common.FizzNetworkException
-import com.harryio.fizz.network.ApiErrorResponse
-import com.harryio.fizz.network.ApiResponse
-import com.harryio.fizz.network.ApiSuccessResponse
 import com.harryio.fizz.network.NetworkInteractor
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import io.reactivex.Single
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.HttpException
-
-fun <T> ApiErrorResponse<T>.errorMsgResId(): Int? = when (errorStatusCode) {
-    8 -> R.string.error_duplicated_entry
-    9 -> R.string.error_service_offline
-    14 -> R.string.error_authentication_failed
-    30 -> R.string.error_invalid_login_credentials
-    31 -> R.string.error_account_disabled
-    32 -> R.string.error_account_not_verified
-    33 -> R.string.error_invalid_request_token
-    34 -> R.string.error_resource_unavailable
-    else -> null
-}
-
 
 internal fun ErrorResponse?.errorMsgResId(): Int? = when (this?.statusCode) {
     8 -> R.string.error_duplicated_entry
@@ -35,26 +18,6 @@ internal fun ErrorResponse?.errorMsgResId(): Int? = when (this?.statusCode) {
     33 -> R.string.error_invalid_request_token
     34 -> R.string.error_resource_unavailable
     else -> null
-}
-
-inline fun <T> Single<ApiResponse<T>>.handleResponse(
-    crossinline successResponseHandler: (response: ApiSuccessResponse<T>) -> T = {
-        it.body ?: throw Exception("Received empty/null response for successful call")
-    },
-    crossinline errorResponseHandler: (response: ApiErrorResponse<T>) -> T = {
-        throw FizzNetworkException(
-            it.errorMsgResId(),
-            it.networkStatusCode,
-            it.cause
-        )
-    }
-): Single<T> {
-    return map { response ->
-        when (response) {
-            is ApiSuccessResponse -> successResponseHandler(response)
-            is ApiErrorResponse -> errorResponseHandler(response)
-        }
-    }
 }
 
 suspend fun <T> makeApiCall(apiCall: suspend () -> T): T {
